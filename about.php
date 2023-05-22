12d0d1bd34f2b40d506f911fdcbbdb0770afb450
WillyAgustri <willyofficial082@gmail.com>
2023-05-23 01:32:54 +0700

    First Commit

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Kelompok Cake & Bakery adalah toko roti yang dibuat untuk tugas database .</p>
+<p>
+ Didirikan pada tahun 2023, saat ini dikelola di bawah PT. Mustika Citra Rasa. Produk kami sehat, bergizi, dan terjangkau oleh semua orang.</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
